eb7b2a4de28d9714462f57370ce2c6ca114a1dec
Dennis Velasquez <dvelasquez2208@gmail.com>
2025-02-06 13:56:08 +0000

    Initial commit - DevSync frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from './components/Logo';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Logo />
+                <h1>Welcome to DevSync</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/logo.png'; // Import the logo
+
+const Logo = () => {
+    return (
+        <img src={logo} alt="DevSync Logo" style={{ width: '150px' }} />
+    );
+};
+
+export default Logo;
